6d1a6ea2f4c967076efd5828b1dbd6a4b8c0bdbe
Guiheux Steven <steven.guiheux+github@gmail.com>
2019-03-07 20:15:38 +0100

    add json annotation

--- types.go
+++ types.go
@@ -1,20 +1,20 @@
 package main
 
 type Data struct {
-	WorkflowName string
-	Number       int64
-	SubNumber    int64
-	NodeName     string
-	Stages       []StageData
+	WorkflowName string      `json:"workflow_name"`
+	Number       int64       `json:"num"`
+	SubNumber    int64       `json:"sub_num"`
+	NodeName     string      `json:"node_name"`
+	Stages       []StageData `json:"stages"`
 }
 
 type StageData struct {
-	Jobs   []JobData
-	Name   string
-	Status string
+	Jobs   []JobData `json:"jobs"`
+	Name   string    `json:"name"`
+	Status string    `json:"status"`
 }
 
 type JobData struct {
-	Status string
-	Name   string
+	Status string `json:"status"`
+	Name   string `json:"name"`
 }
