44d85e872e50706e34ea6de7232f5791a4bcffc5
Steven Guiheux <steven.guiheux@corp.ovh.com>
2019-03-13 20:25:54 +0100

    add log

--- cdsWorkflowRunEvents.go
+++ cdsWorkflowRunEvents.go
@@ -103,12 +103,14 @@ func computeEvent(ctx context.Context, chanSSE <-chan cdsclient.SSEvent, store *
 					continue
 				}
 			case "sdk.EventRunWorkflowNode":
+				cacheKey := fmt.Sprintf("%s-%d-%s", e.WorkflowName, e.WorkflowRunNum, e.WorkflowName)
+				fmt.Printf("%s\n", cacheKey)
 				var eventNR sdk.EventRunWorkflowNode
 				if err := mapstructure.Decode(e.Payload, &eventNR); err != nil {
 					fmt.Printf("unable to read payload of EventRunWorkflowNode: %v  %+v", err, e.Payload)
 					continue
 				}
-				cacheKey := fmt.Sprintf("%s-%d-%s", e.WorkflowName, e.WorkflowRunNum, eventNR.NodeName)
+
 				data := transform(e, eventNR)
 				store.Set(cacheKey, &data, 24*time.Hour)
 			}
